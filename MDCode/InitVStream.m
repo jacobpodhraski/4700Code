48844e5b47c0395f544b5c5021ff327f82bb41a1
jacobpodhraski <jacobpodhraski@cmail.carleton.ca>
2018-01-18 17:38:07 -0500

    Made a few changes

--- MDCode/InitVStream.m
+++ MDCode/InitVStream.m
@@ -4,7 +4,7 @@ TStop = 3000 * dt;
 InitDist = 0.0;
 Method = 'VE'; % VE -- verlot; FD -- Forward Difference
 
-Mass0 = 14 * C.am; % Silicon
+Mass0 = 10 * C.am; % Silicon
 Mass1 = 100 * C.am; % Argon
 
 AtomSpacing = 0.5430710e-9;
@@ -15,15 +15,17 @@ PhiCutoff = 3 * AtomSpacing * 1.1;
 
 T = 30;
 
-AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
+AddCircAtomicArray(4 , 0, 0 , 0 ,0,0,0,0);
+
+%AddRectAtomicArray(10, 10, 0, 0, 0, 0, 0, T, 0);
 % vy0 = -sqrt(0.02*Ep/Mass1);
 % AddRectAtomicArray(4,4,0,12*AtomSpacing,0,vy0,0,T,1);
 Ep = 2;
-AddParticleStream(5, 0.1, 10, -pi / 2, 1, Ep * C.q_0, 5);
+AddParticleStream(20, 0.1, -10, pi/2 , 1, Ep * C.q_0, 1);
 
 Size = 10*AtomSpacing;
 Limits = [-Size +Size -Size +Size]; % square is good
-PlDelt = 5 * dt;
+PlDelt = 10 * dt;
 
 PlotFile = 'BlockSt.gif';
 PlotPosOnly = 1;
--- MDCode/InitVStreamHCP.m
+++ MDCode/InitVStreamHCP.m
@@ -15,11 +15,11 @@ PhiCutoff = 3 * AtomSpacing * 1.1;
 
 T = 30;
 
-AddHCPAtomicBlob(10, 0, 0, 0, 0, 0, 0, T, 0);
+AddHCPAtomicBlob(5, 0, 0, 0, 0, 0, 0, T, 0);
 % vy0 = -sqrt(0.02*Ep/Mass1);
 % AddRectAtomicArray(4,4,0,12*AtomSpacing,0,vy0,0,T,1);
 Ep = 0.5;
-AddParticleStream(13, 0.1, 8, -pi / 2, 1, Ep * C.q_0, 5);
+AddParticleStream(20, 0.1, 8, -pi / 2, 1, Ep * C.q_0, 5);
 
 Size = 8 * AtomSpacing;
 Limits = [-Size +Size -Size +Size]; % square is good
