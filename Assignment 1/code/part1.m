d7bc19e5e977fa4ddd076c3ab654500a7558baf0
jacobpodhraski <jacobpodhraski@cmail.carleton.ca>
2018-02-04 15:48:20 -0500

    Assingment 1 completed Jacob Podhraski 100970231

--- Assignment 1/code/part1.m
+++ Assignment 1/code/part1.m
@@ -19,14 +19,13 @@ kb = 1.38*10^-23;
 
 
 amount = 10;
-v = sqrt((2*kb*temp)/mn);
+v = sqrt((2*kb*temp)/mn)
 
+time = 0;
 colour = hsv(30);
 
-tmn = 0.2*10^-12;
-pscat = 1 - exp(-0.00000000000001/tmn);
 
-velocity = zeros(1,30);
+
 while i<=30
     j = 1;
     while j<=4
@@ -48,32 +47,28 @@ while i<=30
             vy = (v/sqrt(2))*randvy;
             vth = sqrt(vx^2+vy^2);
             init(i,j) = vth;
-            velocity(1,i) = init(i,j)/sqrt(2);
+            
+            
           
-           
+            
         end
         
         j = j + 1;
         
-        
-    end
-    i = i + 1;
+    end        
+    i = i +1;
 end
 
-  figure(1);
-  hist(velocity, 100);
-  title 'Histogram of Velocities';
 
 timestep = 0.00000000000001;
 finaltime = 0.000000000001;
 total = finaltime/timestep;
 T = zeros(1,total);
 counter = zeros(total);
-velocity2 = zeros(1,total);
-figure(2);
+%     figure(2);
 for i =0:timestep:finaltime
-    counter(1,:) = timestep + timestep;
-    
+      counter(1,:) = timestep + timestep;
+  
     
     for j=1:1:30
         
@@ -97,22 +92,13 @@ for i =0:timestep:finaltime
         end
         
         
-       if pscat > rand()
-           randnew = rand(1,1);
-           init(j,3) = randnew * 2 * pi;
-           randvx = randn(1,1);
-            vx = (v/sqrt(2))*randvx;
-            randvy = randn(1,1);
-            vy = (v/sqrt(2))*randvy;
-            vth = sqrt(vx^2+vy^2);
-            init(j,4) = vth;
-       end    
-        
         
         if i~=0
             plot([previous(j,1),init(j,1)],[previous(j,2),init(j,2)],'color',colour(j,:)); %initialize positions
             axis([0 200*10^-9 0 100*10^-9]);
-         
+             
+            
+            
         end
     end
     previous (:,1) = init(:,1);
@@ -120,10 +106,20 @@ for i =0:timestep:finaltime
     
     init(:,1) = init(:,1) + init(:,4).*cos(init(:,3)).*timestep; %moving x components
     init(:,2) = init(:,2) + init(:,4).*sin(init(:,3)).*timestep;
-    
-
-    title 'Part 2: 2-D Trajectories of Electrons with Scatter';
+  
+    T(1,:) = v^2 *mn /(2* kb);
+   
+   figure (1);
+    title 'Part 1: 2-D Trajectories of Electrons';
     hold on;
     pause(0.001);
+    
+ 
 end
 
+    figure(2);
+    plot(counter,T);
+    title 'Temperature Plot over time';
+    ylabel 'Temperature (K)';
+    xlabel 'time (s)';
+    
